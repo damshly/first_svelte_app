2f13ed473c09154a187bbad62100e00d4fc2e6f5
damshly <damshly@proton.me>
2025-12-03 03:30:29 +0400

    create first svelte component

---
+++ src/lib/components/+Button.svelte
@@ -0,0 +1,22 @@
+<script type="ts">
+</script>
+
+<button on:click>
+    <slot>hi</slot>
+</button>
+
+<style>
+    button {
+        padding: 0.5em 1em;
+        border: none;
+        background-color: #6200ee;
+        color: white;
+        border-radius: 4px;
+        cursor: pointer;
+        font-size: 1em;
+    }
+
+    button:hover {
+        background-color: #3700b3;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,6 @@
 <script>
-    let count = $state(0)
-
-    $effect(() => {
-        console.log(count);
-        
-    })
-    let dubled = $derived(count * 2)
-    let dubled2 = count * 2
+    import Button from "$lib/components/+Button.svelte";
 </script>
-
-<button onclick={() => {count++
-    dubled2 = count * 2
-}}>click</button>
-<h1>{dubled}</h1>
-<h1>{dubled2}</h1>
-<h1>{count}</h1>
-<button onclick={() => {
-    count = 0
-}} >reset</button>
+<div>
+    <Button />
+</div>
